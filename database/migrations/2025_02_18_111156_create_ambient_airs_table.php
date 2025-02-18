df034cf06ab6ba2bff61cd0f034c6e3d7a622067
Zidannn10 <zidanazzahra916@gmail.com>
2025-02-18 12:04:53 +0700

    edit table ambient airs

--- database/migrations/2025_02_18_111156_create_ambient_airs_table.php
+++ database/migrations/2025_02_18_111156_create_ambient_airs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ambient_air', function (Blueprint $table) {
+        Schema::create('ambient_airs', function (Blueprint $table) {
             $table->id();
             $table->string('testing_result');
             $table->unsignedBigInteger('parameter_id');
