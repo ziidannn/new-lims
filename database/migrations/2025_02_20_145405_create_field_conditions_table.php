24afcef141171870808cafe6057addbed402c583
Zidannn10 <zidanazzahra916@gmail.com>
2025-03-11 22:57:16 +0700

    benerin error migration field condition

--- database/migrations/2025_02_20_145405_create_field_conditions_table.php
+++ database/migrations/2025_02_20_145405_create_field_conditions_table.php
@@ -14,16 +14,16 @@ return new class extends Migration
         Schema::create('field_conditions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('result_id')->nullable();
-            $table->foreign('rettingult_id')->references('id')->on('results')->nullable();
-            $table->string(column: 'coordinate')->nullable; // Bisa disesuaikan dengan format koordinat
-            $table->string('temperature')->nullable;
-            $table->string('pressure')->nullable;
-            $table->string('humidity')->nullable;
-            $table->string('wind_speed')->nullable;
-            $table->string('wind_direction')->nullable;
-            $table->string('weather')->nullable; // Bisa diubah jika membutuhkan lebih banyak informasi
-            $table->timestamp('created_at');
-            $table->timestamp('updated_at');
+            $table->foreign('result_id')->references('id')->on('results')->nullable();
+            $table->string(column: 'coordinate')->nullable(); // Bisa disesuaikan dengan format koordinat
+            $table->string('temperature')->nullable();
+            $table->string('pressure')->nullable();
+            $table->string('humidity')->nullable();
+            $table->string('wind_speed')->nullable();
+            $table->string('wind_direction')->nullable();
+            $table->string('weather')->nullable(); // Bisa diubah jika membutuhkan lebih banyak informasi
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
         });
     }
 
